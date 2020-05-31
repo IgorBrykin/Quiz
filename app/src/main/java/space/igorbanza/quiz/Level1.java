9d9a917f86a8fa0d67b6c1fa3e7dcf35868c2d5e
IgorBrykin <igorbrikin@gmail.com>
2020-05-31 19:43:39 +0300

    Level1

--- app/src/main/java/space/igorbanza/quiz/Level1.java
+++ app/src/main/java/space/igorbanza/quiz/Level1.java
@@ -3,6 +3,8 @@ package space.igorbanza.quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Level1 extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class Level1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
+
+        // Развернуть игру на весь экран - начало
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        // Раэвернуть игру на весь экран - конец
+
     }
 }
