ac0342d5f0dec08e0b621d021538887ceb070f83
IgorBrykin <igorbrikin@gmail.com>
2020-06-06 16:11:51 +0300

    Level1

--- app/src/main/java/space/igorbanza/quiz/GameLevels.java
+++ app/src/main/java/space/igorbanza/quiz/GameLevels.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -34,6 +35,26 @@ public class GameLevels extends AppCompatActivity {
                 }
             }
         });
+
+        // Кнопка для перехода на первый уровень - начало
+
+        TextView textView1 = (TextView)findViewById(R.id.textView1);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent  = new Intent(GameLevels.this, Level1.class);
+                    startActivity(intent);finish();
+
+                }catch (Exception e){
+
+                }
+            }
+        });
+
+        // Кнопка для перехода на первый уровень - конец
+
+
     }
 
                  // системная кнопка "Назад"  -  начало
